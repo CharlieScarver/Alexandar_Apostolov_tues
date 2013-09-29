ea7343eaf6875c4ec2b8ac7f8d5990b461564344
Alexandar Apostolov <s.apostolovd@gmail.com>
2013-09-30 00:10:06 +0300

    Organization
    ToP & OOP folders for all homeworks

---
+++ Oject-Oriented Programming/Objects & Methods.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class point {
+
+    double x_;
+    double y_;
+
+public:
+
+    point (double x; double y) {
+        x_ = x;
+        y_ = y;
+    }
+
+    double get_x() {
+        return x_;
+    }
+
+    double get_y() {
+        return y_;
+    }
+
+};
+
+
+int main () {
+
+
+
+}
